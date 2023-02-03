e158437f21606ad7699c2c8566adb8a707007877
Koizumi Yuhi <bp18041@shibaura-it.ac.jp>
2023-02-03 15:20:23 +0900

    キーボードでのプレーヤーの移動

--- CoinCollector/__init__.py
+++ CoinCollector/__init__.py
@@ -9,8 +9,11 @@ SCREEN = pygame.Rect(0, 0, 800, 600)
 
 # プレーヤー
 class Player(pygame.sprite.Sprite):
+    # 移動する速度
+    speed = 10
+
     def __init__(self, filename, size):
-        super().__init__()
+        super().__init__(self.containers)
         # 画像を読み込む
         image = pygame.image.load(filename).convert()
         self.image = pygame.transform.scale(image, size)
@@ -18,11 +21,16 @@ class Player(pygame.sprite.Sprite):
         self.image.set_colorkey((0,0,0))
         # Rect（四角）オブジェクトも生成しておく
         self.rect = self.image.get_rect()
+    def move(self, right, down):
+        # プレーヤーを移動させる
+        self.rect.move_ip(right * self.speed, down * self.speed)
+        # 衝突時の処理
+        self.rect = self.rect.clamp(SCREEN)
 
 # コイン
 class Coin(pygame.sprite.Sprite):
     def __init__(self, filename, size):
-        super().__init__()
+        super().__init__(self.containers)
         # 画像を読み込む
         image = pygame.image.load(filename).convert()
         self.image = pygame.transform.scale(image, size)
@@ -41,25 +49,39 @@ def main() -> None:
     screen = pygame.display.set_mode(SCREEN.size)
     pygame.display.set_caption("Coin Collector Game")
     # スプライトでまとめる
-    player_sprites = pygame.sprite.Group()
+    all_sprites = pygame.sprite.RenderUpdates()
     coin_sprites = pygame.sprite.Group()
+    Player.containers = all_sprites
+    Coin.containers = coin_sprites, all_sprites
     # プレーヤーを作る
     player = Player("img/player.png", [100, 100])
-    # プレーヤーをグループに追加する
-    player_sprites.add(player)
     # 3回繰り返す
     for _ in range(3):
         # コインを作る
         coin = Coin("img/coin.png", [80, 80])
         # コインをグループに追加する
         coin_sprites.add(coin)
-    # グループを描画
-    player_sprites.draw(screen)
-    coin_sprites.draw(screen)
+    # クロック
+    clock = pygame.time.Clock()
     while True:
+        # フレームレートの設定
+        clock.tick(60)
+        # スプライトを更新
+        all_sprites.update()
+        screen.fill([0, 0, 0])
+        dirty = all_sprites.draw(screen)
+        # 画面更新
+        pygame.display.update()
         # 終了させる
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-        pygame.display.update()
+        # キー操作の情報を受け取る
+        keystate = pygame.key.get_pressed()
+        # 右方向に移動する距離
+        right = keystate[pygame.K_RIGHT] - keystate[pygame.K_LEFT]
+        # 下方向に移動する距離
+        down = keystate[pygame.K_DOWN] - keystate[pygame.K_UP]
+        # 移動させる
+        player.move(right, down)
 
